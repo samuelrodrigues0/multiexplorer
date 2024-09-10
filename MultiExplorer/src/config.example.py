5d4c3d3dcfed088e944a31eb9ac24a48a1c24366
samuelrodrigues0 <ssrodriguesw@gmail.com>
2024-09-10 07:55:37 -0400

    update config.py

--- MultiExplorer/src/config.example.py
+++ MultiExplorer/src/config.example.py
@@ -1,9 +1,6 @@
 PATH_REPO = '/multiexplorer'  # dir in the virtual environment (Docker set up)
 PATH_ME = '/multiexplorer/MultiExplorer'  # dir in the virtual environment (Docker set up)
 
-# PATH_REPO = '/your/local/dir/multiexplorer'  # dir in your physical machine (native set up)
-# PATH_ME = '/your/local/dir/multiexplorer/MultiExplorer'  # dir in your physical machine (native set up)
-
 PATH_SNIPER = PATH_REPO + '/snipersim-8.0'
 PATH_RUNDIR = PATH_REPO + '/rundir'
 PATH_INPUTS = PATH_REPO + '/input-examples'
@@ -11,3 +8,7 @@ PATH_INPUTS = PATH_REPO + '/input-examples'
 PATH_MCPAT = PATH_ME + '/support/mcpat'
 PATH_RES = PATH_ME + '/res'
 PATH_IMG = PATH_RES + '/img'
+
+PATH_PRED_VM = PATH_ME + "/src/MultiExplorerVM/DS_DSE/predictors/"
+GPU_PATH = PATH_REPO + "/gpgpu-sim_distribution"
+GPU_DB = PATH_ME + '/src/MultiexplorerGPGPU/DS_DSE/db/Experimentos'
